ba90a4e35e01caf63b5ace4e2e6dae3300ee286f
Itsonedev <devonbrownjr24@gmail.com>
2023-10-03 10:17:43 -0400

    Add Codecademy code along

---
+++ src/main/java/codeCademy/ToDoList.java
@@ -0,0 +1,15 @@
+package codeCademy;
+import java.util.ArrayList;
+public class ToDoList {
+    public static void main(String[] args) {
+        ArrayList<String> toDoList = new ArrayList<String>();
+
+        String toDo1 = "Work out";
+        String toDo2 = "Wash Car";
+        String toDo3 = "Get Groceries";
+
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+    }
+}
